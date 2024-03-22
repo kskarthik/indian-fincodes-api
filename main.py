854ec164e0da1fd2f549e906dca21c5aa3cff0ed
Sai Karthik <kskarthik@disroot.org>
2024-03-22 23:37:55 +0530

    update code to support indexing with meilisearch

--- main.py
+++ main.py
@@ -4,17 +4,23 @@
 License: GPLv3 https://www.gnu.org/licenses/gpl-3.0-standalone.html
 
 Summary:
-========
-Convert the hsn/sac spreadhseet provided by the GST portal
-into valid json file
+======== Convert the hsn/sac spreadhseet provided by the GST portal into valid json file
 
 Author
 ============
 Sai Karthik <kskarthik@disroot.org>
 """
 
-import io, json, openpyxl, os, shutil, csv
+import time
+import io
+import json
+import meilisearch
+import openpyxl
+import os
+import shutil
+import csv
 import requests as r
+import requests
 
 print("📡 Getting HSN/SAC file from GST portal ...")
 
@@ -27,11 +33,13 @@ hsn_dict = {}
 sac_dict = {}
 
 portal = "https://www.gst.gov.in/fomessage/newsupdates/"
-# use custom user agent, as the gst website does not seem to allow other programs
-# to access their public api
+# use custom user agent, as the gst portal allow other programs
+# to access their api
 custom_headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
 }
+
+client = meilisearch.Client("http://127.0.0.1:7700", "masterKey")
 
 
 def export_json():
@@ -83,12 +91,15 @@ def export_csv():
     with open("public/hsn.csv", "w", newline="") as csvfile:
         w = csv.writer(csvfile)
         for i in hsn.values:
-            w.writerow([i[0], i[1]])
+            if i[0] == "HSN Code":
+                w.writerow(["HSN", "Desciption"])
+            else:
+                w.writerow([i[0], str(i[1])])
 
     with open("public/sac.csv", "w", newline="") as csvfile:
         w = csv.writer(csvfile)
         for i in sac.values:
-            w.writerow([i[0], i[1]])
+            w.writerow([i[0], str(i[1])])
     print("Created CSV files")
 
 
@@ -120,6 +131,89 @@ def generate_summary():
         print("failed to get summary")
 
 
-export_json()
-export_csv()
-generate_summary()
+def index_ifsc_codes():
+    "Checks for latest release of IFSC codes from razorpay/ifsc repo & downloads the latest one"
+    try:
+        api = "https://api.github.com/repos/razorpay/ifsc/releases?per_page=1"
+
+        print("📡Fetching latest IFSC release ...")
+
+        r = requests.get(api)
+
+        if r.status_code == 200:
+            body = r.json()
+
+            latest_csv_url = body[0]["assets"][3]["browser_download_url"]
+
+            print("⚙ Downloading", body[0]["tag_name"])
+
+            r = requests.get(latest_csv_url)
+
+            if r.status_code == 200:
+                try:
+                    raw_data = csv.DictReader(io.StringIO(r.text))
+                    csv_list = []
+                    for bank in raw_data:
+                        csv_list.append(bank)
+
+                    client.index("banks").add_documents(csv_list, primary_key="IFSC")
+                    print(f"Indexed {len(csv_list)} IFSC codes")
+                except Exception as e:
+                    print("indexing err:", e)
+            else:
+                print("failed to download the csv")
+        else:
+            print("failed to connect with github API")
+
+    except Exception as e:
+        print(e)
+
+
+def index_pin_codes():
+    with open("pincode.csv", "rt") as f:
+        codes = csv.DictReader(f)
+        pincode_list = []
+        count: int = 0
+        for i in codes:
+            i["id"] = count
+            count += 1
+            pincode_list.append(i)
+        client.index("pincodes").add_documents(pincode_list, primary_key="id")
+        print(f"Indexed {len(pincode_list)} pin codes")
+
+
+def index_hsn_sac_codes():
+    all_codes: list = []
+    count: int = 0
+    for i in hsn.values:
+        if i[0] != "HSN Code":
+            all_codes.append({"id": count, "code": i[0], "desciption": i[1]})
+            count += 1
+
+    for i in sac.values:
+        if i[0] != "SAC Code":
+            all_codes.append({"id": count, "code": i[0], "desciption": i[1]})
+            count += 1
+
+    client.index("hsn_sac_codes").add_documents(all_codes, primary_key="id")
+
+    print(f"Indexed {len(all_codes)} HSN/SAC codes")
+    time.sleep(5)
+    # create_dump()
+
+
+def create_dump():
+    try:
+        client.create_dump()
+        print("creating dump")
+        return
+    except Exception as e:
+        print(e)
+
+
+# export_json()
+# export_csv()
+# generate_summary()
+index_ifsc_codes()
+index_pin_codes()
+index_hsn_sac_codes()
