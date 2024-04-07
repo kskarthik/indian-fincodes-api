d4c92e08b5f19cc00965efe9622fbff93434ffe8
Sai Karthik <kskarthik@disroot.org>
2024-04-07 12:14:11 +0530

    feat: add more api tests

--- test_apis.py
+++ test_apis.py
@@ -2,6 +2,12 @@ import meilisearch as ms
 
 client = ms.Client('http://localhost:7700')
 
+pincode_keys = ['id', 'Circle Name', 'Region Name', 'Division Name', 'Office Name', 'Pincode', 'OfficeType', 'Delivery', 'District', 'StateName']
+
+bank_keys = ['IFSC', 'BANK', 'BRANCH', 'CENTRE', 'DISTRICT', 'STATE', 'ADDRESS', 'CONTACT', 'IMPS', 'RTGS', 'CITY', 'ISO3166', 'NEFT', 'MICR', 'UPI', 'SWIFT']
+
+hsnsac_keys = ['id', 'code', 'desciption']
+
 def test_index_count():
     assert client.get_indexes()['total'] == 3
 
@@ -9,11 +15,22 @@ def test_search_pincodes():
     hits = client.get_index("pincodes").search("500101")["hits"]
     assert len(hits) > 1
 
+def test_pincode_keys():
+    hits = client.get_index("pincodes").search("500101")["hits"]
+    assert [*hits[0].keys()] == pincode_keys
+
+def test_bank_keys():
+    hits = client.get_index("banks").search("karol bagh")["hits"]
+    assert [*hits[0].keys()] == bank_keys
+
 def test_search_banks():
     hits = client.get_index("banks").search("karol bagh")["hits"]
-    print(hits)
     assert len(hits) > 1
 
 def test_search_hsnsac():
     hits = client.get_index("hsn_sac_codes").search("razor")["hits"]
     assert len(hits) > 1
+
+def test_hsnsac_keys():
+    hits = client.get_index("hsn_sac_codes").search("razor")["hits"]
+    assert [*hits[0].keys()] == hsnsac_keys
